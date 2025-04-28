9d1d8dc9da3ead30a0c72a201510daeb45068318
chartaseus <90235896+chartaseus@users.noreply.github.com>
2025-04-28 19:34:23 +0700

    Starter project

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default defineConfig([
+  stylistic.configs.customize({ semi: true }),
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js,
+      '@stylistic': stylistic,
+    },
+    extends: ['js/recommended'],
+    rules: {
+      '@stylistic/object-property-newline': [
+        'error',
+        { allowAllPropertiesOnSameLine: true },
+      ],
+      '@stylistic/object-curly-newline': ['error', { multiline: true }],
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+]);
